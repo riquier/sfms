32b09b2a954f3ae58d2bd194d7f3bb235f8118b4
Marcus Riquier <marcus.riquier@lumary.com.au>
2019-12-31 13:20:28 +1030

    remove test coverage exclusions in favour of lower coverage

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,12 @@ module.exports = {
   collectCoverage: true,
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.{ts,js}', '!**/node_modules/**'],
-  coveragePathIgnorePatterns: [
-    "/src/index.ts",
-    "/src/cmds",
-  ],
   coverageThreshold: {
     global: {
       branches: 100,
-      functions: 100,
-      lines: -1,
-      statements: -1,
+      functions: -15,
+      lines: -15,
+      statements: -15,
     },
   },
 };
